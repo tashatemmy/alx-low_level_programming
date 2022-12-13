5bd26eb16e4630896b37095a45d0aa44fb9d83b2
tashatemmy <temmydiva21@gmail.com>
2022-12-13 13:26:50 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _lower - checks if a character is lowercase
+ * @c: the character
+ * Return: 1 if letter is lowercase, 0 if not
+ */
+
+int _lower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
