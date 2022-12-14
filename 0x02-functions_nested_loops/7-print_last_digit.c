dc8d2155f140fbcac2619c8d1517a21ad2a0ee07
tashatemmy <temmydiva21@gmail.com>
2022-12-14 08:55:30 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,11 @@
 
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int last_digit = n % 10;
 
-	if (ld < 0)
-		ld *= -1;
+		if (last_digit < 0)
+			last_digit *= -1;
+		_putchar(last_digit + '0');
 
-	_putchar(ld +  '0');
-
-	return (0);
+		return (last_digit);
 }
