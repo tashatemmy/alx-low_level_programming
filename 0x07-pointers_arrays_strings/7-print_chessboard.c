c26fba37f17a17ba5aad2511c53bb1f0fe2aeab4
tashatemmy <temmydiva21@gmail.com>
2023-01-03 12:15:23 -0800

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints a chessboard.
+ * @a: The chessboard to be printed.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = j = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
